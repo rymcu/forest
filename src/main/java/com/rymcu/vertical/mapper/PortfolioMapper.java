74f5878910af932d226cc05c4a5bef7109c7364c
ronger <ronger-x@outlook.com>
2020-04-07 17:32:20 +0800

    :sparkles: 作品集功能

---
+++ src/main/java/com/rymcu/vertical/mapper/PortfolioMapper.java
@@ -0,0 +1,10 @@
+package com.rymcu.vertical.mapper;
+
+import com.rymcu.vertical.core.mapper.Mapper;
+import com.rymcu.vertical.entity.Portfolio;
+
+/**
+ * @author ronger
+ */
+public interface PortfolioMapper extends Mapper<Portfolio> {
+}
