5b66d5623294c9589579a75eb1afd1819da7a32b
ronger <ronger-x@outlook.com>
2020-08-04 17:20:36 +0800

    :art: 菜单路径变更

--- src/main/java/com/rymcu/vertical/wx/mp/controller/WxMenuController.java
+++ src/main/java/com/rymcu/vertical/wx/mp/controller/WxMenuController.java
@@ -67,7 +67,7 @@ public class WxMenuController {
         WxMenuButton button31 = new WxMenuButton();
         button31.setType(MenuButtonType.VIEW);
         button31.setName("51单片机");
-        button31.setUrl("https://rymcu.com/vertical/portfolio/8");
+        button31.setUrl("https://rymcu.com/portfolio/8");
 
 //        WxMenuButton button34 = new WxMenuButton();
 //        button34.setType(MenuButtonType.VIEW);
