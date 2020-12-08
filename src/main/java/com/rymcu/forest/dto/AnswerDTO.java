618a07d435545c96f62a717da47efc2f2323a3c6
ronger <ronger-x@outlook.com>
2020-12-08 20:00:25 +0800

    :sparkles: 每日一题系统

---
+++ src/main/java/com/rymcu/forest/dto/AnswerDTO.java
@@ -0,0 +1,17 @@
+package com.rymcu.forest.dto;
+
+import lombok.Data;
+
+/**
+ * @author ronger
+ */
+@Data
+public class AnswerDTO {
+
+    private Integer idSubjectQuestion;
+
+    private String answer;
+
+    private Integer idUser;
+
+}
