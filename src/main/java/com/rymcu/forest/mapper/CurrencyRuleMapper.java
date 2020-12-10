978e75735ffca68ee63a42b9a761ca0bc267212c
ronger <ronger-x@outlook.com>
2020-12-10 16:24:11 +0800

    :sparkles: 货币规则

---
+++ src/main/java/com/rymcu/forest/mapper/CurrencyRuleMapper.java
@@ -0,0 +1,10 @@
+package com.rymcu.forest.mapper;
+
+import com.rymcu.forest.core.mapper.Mapper;
+import com.rymcu.forest.entity.CurrencyRule;
+
+/**
+ * @author ronger
+ */
+public interface CurrencyRuleMapper extends Mapper<CurrencyRule> {
+}
