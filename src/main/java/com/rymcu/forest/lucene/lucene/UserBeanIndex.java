8e6707f7fd92019df85a85003aa1d677725ac37f
ronger <ronger-x@outlook.com>
2021-04-29 10:18:03 +0800

    :bug: 空指针异常修复

--- src/main/java/com/rymcu/forest/lucene/lucene/UserBeanIndex.java
+++ src/main/java/com/rymcu/forest/lucene/lucene/UserBeanIndex.java
@@ -9,6 +9,7 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -19,29 +20,29 @@ import java.util.concurrent.CountDownLatch;
  */
 public class UserBeanIndex extends BaseIndex<UserLucene> {
 
-  public UserBeanIndex(
-      String parentIndexPath,
-      int subIndex,
-      CountDownLatch countDownLatch1,
-      CountDownLatch countDownLatch2,
-      List<UserLucene> list) {
-    super(parentIndexPath, subIndex, countDownLatch1, countDownLatch2, list);
-  }
-
-  @Override
-  public void indexDoc(IndexWriter writer, UserLucene user) throws Exception {
-    Document doc = new Document();
-    Field id = new Field("id", user.getIdUser() + "", TextField.TYPE_STORED);
-    Field title = new Field("nickname", user.getNickname(), TextField.TYPE_STORED);
-    Field summary = new Field("signature", user.getSignature(), TextField.TYPE_STORED);
-    // 添加到Document中
-    doc.add(id);
-    doc.add(title);
-    doc.add(summary);
-    if (writer.getConfig().getOpenMode() == IndexWriterConfig.OpenMode.CREATE) {
-      writer.addDocument(doc);
-    } else {
-      writer.updateDocument(new Term("id", user.getIdUser() + ""), doc);
+    public UserBeanIndex(
+            String parentIndexPath,
+            int subIndex,
+            CountDownLatch countDownLatch1,
+            CountDownLatch countDownLatch2,
+            List<UserLucene> list) {
+        super(parentIndexPath, subIndex, countDownLatch1, countDownLatch2, list);
     }
-  }
+
+    @Override
+    public void indexDoc(IndexWriter writer, UserLucene user) throws Exception {
+        Document doc = new Document();
+        Field id = new Field("id", user.getIdUser() + "", TextField.TYPE_STORED);
+        Field title = new Field("nickname", user.getNickname(), TextField.TYPE_STORED);
+        Field summary = new Field("signature", Objects.nonNull(user.getSignature()) ? user.getSignature() : "", TextField.TYPE_STORED);
+        // 添加到Document中
+        doc.add(id);
+        doc.add(title);
+        doc.add(summary);
+        if (writer.getConfig().getOpenMode() == IndexWriterConfig.OpenMode.CREATE) {
+            writer.addDocument(doc);
+        } else {
+            writer.updateDocument(new Term("id", user.getIdUser() + ""), doc);
+        }
+    }
 }
