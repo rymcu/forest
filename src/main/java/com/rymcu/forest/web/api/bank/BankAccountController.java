e966cf01f201b48dc453d03a2b4bb49e7081d74e
ronger <ronger-x@outlook.com>
2021-12-28 23:22:52 +0800

    :sparkles: 银行卡管理-增加交易记录查看功能

--- src/main/java/com/rymcu/forest/web/api/bank/BankAccountController.java
+++ src/main/java/com/rymcu/forest/web/api/bank/BankAccountController.java
@@ -6,10 +6,13 @@ import com.rymcu.forest.core.result.GlobalResult;
 import com.rymcu.forest.core.result.GlobalResultGenerator;
 import com.rymcu.forest.dto.BankAccountDTO;
 import com.rymcu.forest.dto.BankAccountSearchDTO;
+import com.rymcu.forest.dto.TransactionRecordDTO;
 import com.rymcu.forest.service.BankAccountService;
+import com.rymcu.forest.util.Utils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,6 +46,21 @@ public class BankAccountController {
     public GlobalResult detail(@PathVariable Integer idUser) {
         BankAccountDTO bankAccount = bankAccountService.findBankAccountByIdUser(idUser);
         return GlobalResultGenerator.genSuccessResult(bankAccount);
+    }
+
+    @GetMapping("/transaction-records")
+    public GlobalResult transactionRecords(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "20") Integer rows, HttpServletRequest request) {
+        String bankAccount = request.getParameter("bankAccount");
+        String startDate = request.getParameter("startDate");
+        String endDate = request.getParameter("endDate");
+        PageHelper.startPage(page, rows);
+        List<TransactionRecordDTO> list = bankAccountService.findUserTransactionRecords(bankAccount, startDate, endDate);
+        PageInfo<TransactionRecordDTO> pageInfo = new PageInfo(list);
+        Map map = new HashMap(2);
+        map.put("records", pageInfo.getList());
+        Map pagination = Utils.getPagination(pageInfo);
+        map.put("pagination", pagination);
+        return GlobalResultGenerator.genSuccessResult(map);
     }
 
 }
