e7e672a7b75cab23f491bd7f28bc3f12c304032a
ronger <ronger-x@outlook.com>
2022-01-04 10:32:56 +0800

    :poop: 修改文章标签进行鉴权

--- src/main/java/com/rymcu/forest/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/rymcu/forest/service/impl/ArticleServiceImpl.java
@@ -1,7 +1,6 @@
 package com.rymcu.forest.service.impl;
 
 import com.rymcu.forest.core.constant.NotificationConstant;
-import com.rymcu.forest.core.constant.ProjectConstant;
 import com.rymcu.forest.core.service.AbstractService;
 import com.rymcu.forest.dto.*;
 import com.rymcu.forest.entity.Article;
@@ -323,7 +322,7 @@ public class ArticleServiceImpl extends AbstractService<Article> implements Arti
         Map map = new HashMap(2);
         Article article = articleMapper.selectByPrimaryKey(idArticle);
         if (Objects.nonNull(article)) {
-            if (isAuthor(article.getArticleAuthorId())) {
+            if (isAuthor(article.getArticleAuthorId()) || hasAdminPermission()) {
                 article.setArticleTags(tags);
                 articleMapper.updateArticleTags(idArticle, tags);
                 tagService.saveTagArticle(article, "");
@@ -337,6 +336,15 @@ public class ArticleServiceImpl extends AbstractService<Article> implements Arti
             map.put("message", "更新失败,文章不存在!");
         }
         return map;
+    }
+
+    private boolean hasAdminPermission() throws BaseApiException {
+        User user = UserUtils.getCurrentUserByToken();
+        if (Objects.nonNull(user)) {
+            Integer userRoleWeight = userService.findRoleWeightsByUser(user.getIdUser());
+            return userRoleWeight <= ADMIN_ROLE_WEIGHTS;
+        }
+        return false;
     }
 
     private boolean isAuthor(Integer idUser) throws BaseApiException {
