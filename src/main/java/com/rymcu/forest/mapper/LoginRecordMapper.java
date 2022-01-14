73f4eaddc5854f521767761862d5a5f156a330d8
ronger <ronger-x@outlook.com>
2022-01-14 11:59:57 +0800

    :sparkles: 用户登录日志查询接口

--- src/main/java/com/rymcu/forest/mapper/LoginRecordMapper.java
+++ src/main/java/com/rymcu/forest/mapper/LoginRecordMapper.java
@@ -2,6 +2,9 @@ package com.rymcu.forest.mapper;
 
 import com.rymcu.forest.core.mapper.Mapper;
 import com.rymcu.forest.entity.LoginRecord;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * Created on 2022/1/14 8:46.
@@ -11,4 +14,10 @@ import com.rymcu.forest.entity.LoginRecord;
  * @packageName com.rymcu.forest.mapper
  */
 public interface LoginRecordMapper extends Mapper<LoginRecord> {
+    /**
+     * 获取用户登录记录
+     * @param idUser
+     * @return
+     */
+    List<LoginRecord> selectLoginRecordByIdUser(@Param("idUser") Integer idUser);
 }
