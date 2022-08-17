97375a3dbd30ef2a5742ddeabb49b2896b34987a
ronger <ronger-x@outlook.com>
2022-08-17 09:52:14 +0800

    :art: 开启异步执行

--- src/main/java/com/rymcu/forest/ForestApplication.java
+++ src/main/java/com/rymcu/forest/ForestApplication.java
@@ -2,10 +2,12 @@ package com.rymcu.forest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author ronger
  */
+@EnableAsync
 @SpringBootApplication
 public class ForestApplication {
 
