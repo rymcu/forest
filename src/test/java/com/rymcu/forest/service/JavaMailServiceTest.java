bd90806460641545efbbbc0b6f482430f802b8dc
你一个人在这儿干嘛你是来拉屎的吧 <1421374934@qq.com>
2023-04-03 22:06:57 +0800

    :bug: 部分测试

--- src/test/java/com/rymcu/forest/service/JavaMailServiceTest.java
+++ src/test/java/com/rymcu/forest/service/JavaMailServiceTest.java
@@ -1,19 +1,36 @@
 package com.rymcu.forest.service;
 
 import com.rymcu.forest.base.BaseServiceTest;
+import com.rymcu.forest.dto.NotificationDTO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.mail.MessagingException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * javaMail测试
+ */
 class JavaMailServiceTest extends BaseServiceTest {
 
+    private static final String REALITY_EMAIL = "1421374934@qq.com";
+    @Autowired
+    private JavaMailService javaMailService;
+
     @Test
-    void sendEmailCode() {
+    void sendEmailCode() throws MessagingException {
+        assertEquals(1, javaMailService.sendEmailCode(REALITY_EMAIL));
     }
 
     @Test
-    void sendForgetPasswordEmail() {
+    void sendForgetPasswordEmail() throws MessagingException {
+        assertEquals(1, javaMailService.sendForgetPasswordEmail(REALITY_EMAIL));
     }
 
     @Test
-    void sendNotification() {
+    void sendNotification() throws MessagingException {
+        assertEquals(0, javaMailService.sendNotification(new NotificationDTO()));
+
     }
 }
