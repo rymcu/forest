b2d4012ea52a0f50a40e80daa065512bbf0a6273
你一个人在这儿干嘛你是来拉屎的吧 <1421374934@qq.com>
2023-04-04 16:14:23 +0800

    :bug: 部分测试

--- src/test/java/com/rymcu/forest/service/JavaMailServiceTest.java
+++ src/test/java/com/rymcu/forest/service/JavaMailServiceTest.java
@@ -4,10 +4,13 @@ import com.rymcu.forest.base.BaseServiceTest;
 import com.rymcu.forest.dto.NotificationDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailParseException;
 
 import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * javaMail测试
@@ -20,12 +23,12 @@ class JavaMailServiceTest extends BaseServiceTest {
 
     @Test
     void sendEmailCode() throws MessagingException {
-        assertEquals(1, javaMailService.sendEmailCode(REALITY_EMAIL));
+        assertThrows(MailParseException.class, () -> javaMailService.sendEmailCode(REALITY_EMAIL));
     }
 
     @Test
     void sendForgetPasswordEmail() throws MessagingException {
-        assertEquals(1, javaMailService.sendForgetPasswordEmail(REALITY_EMAIL));
+        assertThrows(AddressException.class, () -> javaMailService.sendForgetPasswordEmail(REALITY_EMAIL));
     }
 
     @Test
--- src/test/java/com/rymcu/forest/service/PermissionServiceTest.java
+++ src/test/java/com/rymcu/forest/service/PermissionServiceTest.java
@@ -1,16 +1,34 @@
 package com.rymcu.forest.service;
 
 import com.rymcu.forest.base.BaseServiceTest;
+import com.rymcu.forest.entity.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.BadSqlGrammarException;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * 权限测试
+ */
 class PermissionServiceTest extends BaseServiceTest {
+
+    @Autowired
+    private PermissionService permissionService;
 
     @BeforeEach
     void setUp() {
     }
 
     @Test
+    @DisplayName("获取用户权限")
     void selectPermissionByUser() {
+        User user = new User();
+        user.setIdUser(1L);
+        assertThrows(BadSqlGrammarException.class, () -> {
+            permissionService.selectPermissionByUser(user);
+        });
     }
 }
